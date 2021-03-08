233df7252db9ac086c81ed6ca61bbafbe50cb51f
Marcus Aurélio <marcus.oliveira@tvca.com.br>
2021-03-08 17:55:35 -0400

    Initial commit 3

--- src/components/TextInput/index.jsx
+++ src/components/TextInput/index.jsx
@@ -8,7 +8,7 @@ export const TextInput = ({ searchValue, handleChange }) => {
       onChange={handleChange}
       value={searchValue}
       type="search"
-      placeholder="Type your search"
+      placeholder="Digite o que busca"
     />
   );
 };
